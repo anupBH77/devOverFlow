edc059f356430d59506bc9844b38780e730a5802
anupbh77 <anupbhaumik44@gmail.com>
2024-02-07 22:43:21 +0530

    Initial commit from Create Next App

---
+++ utils/constants/TabList.ts
@@ -0,0 +1,12 @@
+import { tabListType } from "../types/tabListType";
+import { TiHomeOutline,TiTag } from "react-icons/ti";
+import { IoPeopleOutline ,IoPricetagOutline} from "react-icons/io5";
+import { BsQuestionSquare } from "react-icons/bs";
+import { BiCollection } from "react-icons/bi";
+const tabList: tabListType[] = [
+    { routename: "Home",routeUrl:"/Home",routeIcon:TiHomeOutline },
+    { routename: "Ask",routeUrl:"/Ask",routeIcon:BsQuestionSquare},
+    { routename: "Tags",routeUrl:"/Tags",routeIcon:IoPricetagOutline},
+    { routename: "Communities",routeUrl:"/Communities",routeIcon:IoPeopleOutline},
+    { routename: "Collections",routeUrl:"/Collections",routeIcon:BiCollection },
+];
---
+++ utils/types/tabListType.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import { IconBaseProps, IconType } from "react-icons";
+
+export interface tabListType{
+    routeUrl:string,
+    routename:string,
+    routeIcon:IconType
+} 
